5414d5f64dffecb209fe804b5886bbccb56b5a94
Max Marrone <max@marrone.nyc>
2025-07-18 00:36:50 -0400

    Optimize bundle size somewhat.
    - Pare down which ag-grid features are included. I'm including some features that we don't actually use yet but probably will later, like TooltipModule. This takes us from something like 1300 KB -> 900 KB uncompressed.
    
    - Use a more granular Lodash import.
    
    - Finally, just increase the bundle size warning threshold, which is like 200 KB by default. It doesn't seem possible to meet the default as long as we use ag-grid.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,15 @@
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import {
+  CellStyleModule,
+  CheckboxEditorModule,
+  ClientSideRowModelModule,
+  ColumnAutoSizeModule,
+  ExternalFilterModule,
+  ModuleRegistry,
+  RowAutoHeightModule,
+  RowStyleModule,
+  TooltipModule,
+  ValidationModule,
+} from "ag-grid-community";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
@@ -6,8 +17,20 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import { store } from "./redux/store.ts";
 
-// TODO: Pare this down to reduce bundle size.
-ModuleRegistry.registerModules([AllCommunityModule]);
+ModuleRegistry.registerModules([
+  CellStyleModule,
+  CheckboxEditorModule,
+  ClientSideRowModelModule,
+  ColumnAutoSizeModule,
+  ExternalFilterModule,
+  RowAutoHeightModule,
+  RowStyleModule,
+  TooltipModule,
+]);
+if (import.meta.env.MODE === "development") {
+  console.debug("Loading ag-grid validation module.");
+  ModuleRegistry.registerModules([ValidationModule]);
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // ag-grid is gigantic, so we need to increase the default limit.
+    chunkSizeWarningLimit: 1200,
+  },
 });
