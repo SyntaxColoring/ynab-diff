cb64b178a7973bf260042ddfb8c5bcac4bd73e56
Max Marrone <max@marrone.nyc>
2025-07-19 18:56:48 -0400

    Silence Redux serializability warnings.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, isPlain } from "@reduxjs/toolkit";
+import currency from "currency.js";
 
 import { currencyFormatSlice } from "./currencyFormatSlice";
 import { tablesSlice } from "./tablesSlice";
@@ -9,10 +10,30 @@ export const store = configureStore({
     currencyFormat: currencyFormatSlice.reducer,
     tables: tablesSlice.reducer,
   },
+
+  // Redux strongly recommends sticking to primitive objects. I think that guideline is dumb --
+  // data structures are good and so are domain-specific types -- so, silence all the warnings
+  // relating to it.
   devTools: {
     serialize: true,
   },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        isSerializable: (value: unknown) => {
+          return isPlain(value) || knownUnserializable(value);
+        },
+        getEntries: (value: object) => {
+          return knownUnserializable(value) ? [] : Object.entries(value);
+        },
+      },
+    });
+  },
 });
+
+function knownUnserializable(value: unknown): boolean {
+  return [currency, Map, Set].some((ctor) => value instanceof ctor);
+}
 
 export type Store = typeof store;
 export type RootState = ReturnType<Store["getState"]>;
